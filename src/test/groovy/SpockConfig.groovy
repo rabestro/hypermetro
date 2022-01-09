99fe340ba8f9babce3c8ffc9f5f7eacd67e78e75
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2022-01-09 20:59:18 +0200

    Add SpockConfig.groovy

---
+++ src/test/groovy/SpockConfig.groovy
@@ -0,0 +1,6 @@
+spockReports {
+    set 'com.athaydes.spockframework.report.showCodeBlocks': true
+    set 'com.athaydes.spockframework.report.outputDir': 'docs/spock-report'
+    set 'com.athaydes.spockframework.report.projectName': 'HyperMetro'
+    set 'com.athaydes.spockframework.report.projectVersion': 1.0
+}
