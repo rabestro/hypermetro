1153714a84fe1501106ca43d0a446362a5833783
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2022-01-22 16:16:42 +0200

    Add ShellCommandsSpec

---
+++ src/test/groovy/metro/service/ShellCommandsSpec.groovy
@@ -0,0 +1,45 @@
+package metro.service
+
+import spock.lang.Specification
+import spock.lang.Subject
+
+class ShellCommandsSpec extends Specification {
+    def metroService = Mock MetroService
+
+    @Subject
+    def commands = new ShellCommands(metroService)
+
+    def "Output"() {
+    }
+
+    def "should execute add-head command"() {
+        given:
+        def metroLine = 'Bakerloo'
+        and:
+        def metroStation = 'Waterloo'
+
+        when: 'we execute the command with these parameters'
+        def output = commands.addHead(metroLine, metroStation)
+
+        then: 'the metro service is called with these parameters'
+        1 * metroService.addHead(metroLine, metroStation)
+
+        and: 'execution of the command is successful'
+        output.contains 'successfully'
+    }
+
+    def "Append"() {
+    }
+
+    def "Connect"() {
+    }
+
+    def "Remove"() {
+    }
+
+    def "Route"() {
+    }
+
+    def "FastestRoute"() {
+    }
+}
