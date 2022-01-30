028a0570b2305e2adb41300ca9ed87e2d1799e5c
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2022-01-30 15:09:59 +0200

    Update Station.java

--- src/main/java/metro/model/Station.java
+++ src/main/java/metro/model/Station.java
@@ -7,6 +7,12 @@ import java.util.Set;
  *
  * Includes the name of the station, a list of next and previous stations,
  * and a list of stations to which a transfer is possible.
+ 
+ * @param name the name of the subway station.
+ * @param time the travel time in minutest to the next station
+ * @param next the set of next stations
+ * @param prev the set of previous stations
+ * @param transfer the set stationId connected to this station
  */
 public record Station(String name, int time, Set<String> next, Set<String> prev, Set<StationId> transfer) {
 }
